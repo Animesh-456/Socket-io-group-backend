bd633775a043f56e70c9c8a869c7daf1d2bf7168
Animesh-456 <anim29006@gmail.com>
2024-05-22 18:29:22 +0530

    modified server config.

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
-import http from 'http';
+import express from 'express';
 import { Server } from 'socket.io';
-import { Stream } from 'stream';
+import http from 'http';
 
-// Create an HTTP server
-const server = http.createServer();
-
-// Create a new instance of Socket.IO
+const app = express();
+const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
         origin: '*',
     },
 });
 
-// Listen for incoming connections
 io.on('connection', (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
@@ -22,49 +19,38 @@ io.on('connection', (socket) => {
         console.log(`User with ID: ${socket.id} joined the room: ${data}`);
     });
 
-
-
     socket.on("leave_room", (data) => {
         socket.leave(data.room);
         socket.to(data.room).emit("user_left", { user: data.user, socketId: socket.id });
         console.log(`User with ID: ${socket.id} left the room: ${data}`);
     });
 
-
-
     socket.on("send_message", (data) => {
         socket.to(data.room).emit("receive_message", data);
-        console.log("message received", data)
+        console.log("message received", data);
     });
-
-
 
     socket.on('call-user', (data) => {
         socket.to(data.socketId).emit('incoming-call', { from: socket.id, offer: data.offer });
     });
 
-
     socket.on("call-accepted", (data) => {
-        //console.log("socket id for accept call", socket.id)
-        socket.to(data.socketId).emit("call-accepted", { answer: data.answer })
-        console.log("call accepted", data.answer)
-    })
-
-    // socket.on('answer', (targetId, answer) => {
-    //     socket.to(targetId).emit('answer', socket.id, answer);
-    // });
-
-    // socket.on('iceCandidate', (targetI, candidate) => {
-    //     io.to(targetId).emit('iceCandidate', socket.id, candidate);
-    // });
+        socket.to(data.socketId).emit("call-accepted", { answer: data.answer });
+        console.log("call accepted", data.answer);
+    });
 
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
     });
 });
 
-// Start the server
-const PORT = 3001;
+app.get('/', (req, res) => {
+    res.send('Socket.IO server running');
+});
+
+const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
     console.log(`Socket.IO server running on port ${PORT}`);
 });
+
+export default app;
